8b7abdf070cd7139ecaba635835e7567c15b1ee2
Sage <salazz@iu.edu>
2022-12-17 15:40:17 -0600

    added key to config

--- manualRun.bat
+++ manualRun.bat
@@ -1,4 +1,4 @@
-powershell python C:\Users\Sage\OneDrive\Desktop\Clans\Clans\main.py manual
+powershell python C:\Users\Sage\OneDrive\Desktop\gitRepos\ClashOfClans_clanManagement\main.py manual
 powershell git pull
 powershell git add Clash.xlsx
 powershell git commit -m 'updating the clash file from bat manual run'
--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-powershell python C:\Users\Sage\OneDrive\Desktop\Clans\Clans\main.py automation
+powershell python C:\Users\Sage\OneDrive\Desktop\gitRepos\ClashOfClans_clanManagement\main.py automation
 powershell git pull
 powershell git add Clash.xlsx
 powershell git commit -m 'updating the clash file from bat automatic run'
